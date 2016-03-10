7959ac8f7be7f8fe73db1c9fee79d29e42a3a56c
Raul Martinez <rmartinez44@islander.tamucc.edu>
2016-03-10 17:46:57 -0600

    Made the first part of the assignment to work

--- shell.c
+++ shell.c
@@ -7,72 +7,63 @@
 #include <stdbool.h>
 #define MAX_LINE 64 /* The maximum length command */
 
-bool getArguments(char *line, char **argv);
-void runArguments(char **argv, int shouldIWait);
+void getArguments(char *line, char **argv);
+void runArguments(char **argv, bool shouldWait);
 
 int main(void) {
     char args[MAX_LINE];             /* the input line                 */
     char *argv[MAX_LINE];        /* the command line argument      */
-	int should_run = 1;           /* flag to determine when to exit program */
+    int should_run = 1;           /* flag to determine when to exit program */
+    bool shouldIWait = true;
 
+    while (should_run) {
+        printf("osh>");
+        gets(args);
 
-	while (should_run) {
-		printf("osh>");
-		gets(args);
-        bool containsAmpersand = getArguments(args, argv);
-		if (strcmp(argv[0], "exit") == 0) {
-			should_run = 0;
-		} else if (containsAmpersand) {
-            runArguments(argv, 0);
-        } else {
-            runArguments(argv, 1);
+        if (strstr(args, "&") != NULL) {
+            shouldIWait = false;
+            int i = strlen(args);
+            args[i-2] = '\0';
+            args[i-1] = '\0';
         }
-	}
-    fflush(stdout);
-	return 0;
-}
-
-bool getArguments(char *line, char **argv) {
-    bool ampersand = false;
-    while (*line != '\0') {
-        while (*line == ' ' || *line == '\t' || *line == '\n') {
-            *line++ = '\0';
-            if (*line == '&')
-            {
-                ampersand = true;
-                *line = '\0';
-            }
+        if (strcmp(args, "exit") == 0) {
+            return 0;
         }
-        *argv++ = line;
-        while (*line != '\0' && *line != ' ' &&
-               *line != '\t' && *line != '\n') {
-            if (*line == '&')
-            {
-                ampersand = true;
-                *line = '\0';
-            } else {
-                line++;
-            }
+        else {
+            getArguments(args,argv);
+            runArguments(argv, shouldIWait);
         }
     }
-    *argv = '\0';
-    return ampersand;
 }
 
-void runArguments(char **argv, int shouldIWait) {
+void getArguments(char *line, char **argv) {
+    while (*line != '\0') {       /* if not the end of line ....... */
+        while (*line == ' ' || *line == '\t' || *line == '\n')
+            *line++ = '\0';     /* replace white spaces with 0    */
+        *argv++ = line;          /* save the argument position     */
+        while (*line != '\0' && *line != ' ' &&
+               *line != '\t' && *line != '\n')
+            line++;             /* skip the argument until ...    */
+    }
+    *argv = '\0';                 /* mark the end of argument list  */
+}
+
+void runArguments(char **argv, bool shouldWait) {
     pid_t  pid;
     int    status;
 
     if ((pid = fork()) < 0) {     /* fork a child process           */
         printf("*** ERROR: forking child process failed\n");
         exit(1);
-    } else if (pid == 0) {          /* for the child process:         */
+    }
+    else if (pid == 0) {          /* for the child process:         */
         if (execvp(*argv, argv) < 0) {     /* execute the command  */
             printf("*** ERROR: exec failed\n");
             exit(1);
         }
-    } else {                                  /* for the parent:      */
-        if (shouldIWait != 0) {
+    }
+    else {                                  /* for the parent:      */
+        if(shouldWait) {
             while (wait(&status) != pid) { }       /* wait for completion  */
         }
     }
