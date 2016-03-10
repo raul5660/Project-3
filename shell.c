c8d02a63f7dd8d00c5cba3e6241b550abf78db84
Raul Martinez <rmartinez44@islander.tamucc.edu>
2016-03-10 15:56:58 -0600

    FUCK

--- shell.c
+++ shell.c
@@ -15,14 +15,14 @@ int main(void) {
     char *argv[MAX_LINE];        /* the command line argument      */
 	int should_run = 1;           /* flag to determine when to exit program */
 
+
 	while (should_run) {
 		printf("osh>");
 		gets(args);
         bool containsAmpersand = getArguments(args, argv);
-
 		if (strcmp(argv[0], "exit") == 0) {
 			should_run = 0;
-		} else if (containsAmpersand == 1) {
+		} else if (containsAmpersand) {
             runArguments(argv, 0);
         } else {
             runArguments(argv, 1);
@@ -36,7 +36,12 @@ bool getArguments(char *line, char **argv) {
     bool ampersand = false;
     while (*line != '\0') {
         while (*line == ' ' || *line == '\t' || *line == '\n') {
-                *line++ = '\0';
+            *line++ = '\0';
+            if (*line == '&')
+            {
+                ampersand = true;
+                *line = '\0';
+            }
         }
         *argv++ = line;
         while (*line != '\0' && *line != ' ' &&
@@ -66,7 +71,9 @@ void runArguments(char **argv, int shouldIWait) {
             printf("*** ERROR: exec failed\n");
             exit(1);
         }
-    } else if (shouldIWait != 0) {                                  /* for the parent:      */
-        while (wait(&status) != pid) {}       /* wait for completion  */
+    } else {                                  /* for the parent:      */
+        if (shouldIWait != 0) {
+            while (wait(&status) != pid) { }       /* wait for completion  */
+        }
     }
 }
